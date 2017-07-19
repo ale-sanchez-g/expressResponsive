b1919347a54116a3be3fdf02ee8e7855f4876775
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-07-19 18:08:17 +1000

    adding password form (#4)

--- app/routes/password.js
+++ app/routes/password.js
@@ -3,21 +3,7 @@ var request = require('request');
 exports.password = function(req, res){
     var leng = req.params.leng;
 
-    switch (leng) {
-        case "english":
-            apiTag = "password";
-            break;
-        case "spanish":
-            apiTag = "palabras";
-            break;
-        case "german":
-            apiTag = "worts";
-            break;
-        default:
-            apiTag = "help";
-
-    }
-        request.get('https://guarded-scrubland-78590.herokuapp.com/' + apiTag, function(err, response, body) {
+        request.get('https://guarded-scrubland-78590.herokuapp.com/' + leng, function(err, response, body) {
             if (!err && response.statusCode == 200) {
                 var locals = JSON.parse(body);
                 res.render('password', locals);
